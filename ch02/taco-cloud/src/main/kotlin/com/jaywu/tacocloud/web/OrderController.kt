1191afebcc2c25cdbb4e02eecad1e82b7550cf09
jay_wu <jay_wu@gss.com.tw>
2020-10-16 19:07:49 +0800

    2. add order form

--- ch02/taco-cloud/src/main/kotlin/com/jaywu/tacocloud/web/OrderController.kt
+++ ch02/taco-cloud/src/main/kotlin/com/jaywu/tacocloud/web/OrderController.kt
@@ -5,6 +5,7 @@ import mu.KotlinLogging
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 private val logger = KotlinLogging.logger {}
@@ -19,5 +20,10 @@ class OrderController {
         return "orderForm"
     }
 
+    @PostMapping
+    fun processOrder(order: Order): String {
+        logger.info { "Order submitted: $order" }
+        return "redirect:/"
+    }
 
 }
